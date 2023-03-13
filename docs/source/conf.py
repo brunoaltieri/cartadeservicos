becb31e6a56d417582eb10442d1c45bb00e60b0c
Bruno Altieri <nahas.altieri@gmail.com>
2023-03-13 16:43:12 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Carta de Serviços ao Cidadão | SC'
+copyright = '2023, DITI|SEA'
+author = 'Bruno Altieri'
 
 release = '0.1'
 version = '0.1.0'
